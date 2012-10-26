64a8f6a6097ee90f956a1e7a3d1fa24dc03a11b9
Leon Lin <leonlin@leonspro-2.local>
2012-10-25 20:17:14 -0700

    added the footer portion

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
 
-	<div data-role="footer" data-position="fixed">
-		<div data-role="navbar" data-grid="a">
+<div data-role="footer" data-position="fixed">
+	<div data-role="navbar">
 		<ul>
-			<li><a href="#" data-icon="plus" data-theme="a">Raise an Issue</a></li>
-			<li><a href="options.php" data-icon="gear" data-theme="a">Options</a></li>
+			<li><a href="#" data-icon="plus" data-theme="a">Issues</a></li>
+			<li><a href="options.php" data-icon="gear" data-theme="a">Politicians</a></li>
+			<li><a href="options.php" data-icon="gear" data-theme="a">Nearby</a></li>
+			<li><a href="options.php" data-icon="gear" data-theme="a">Add Issue</a></li>
 		</ul>
 	</div>
 
