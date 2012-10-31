1a74e9fe36a75a6fd9dca3e107f70b0428fdf07b
leonlin <leonlin@stanford.edu>
2012-10-31 16:33:27 -0700

    modified some things related to login...need to commit to do something

--- login.php
+++ login.php
@@ -2,25 +2,25 @@
 <!-- /header -->
 
 <div data-role="content">	
-	<?php 
-		if (isset($_POST["logout"]) && strcmp($_POST["logout"],"true") == 0){
-			$_SESSION["username"] = null;
-	?>
+<?php 
+if (isset($_POST["logout"]) && strcmp($_POST["logout"],"true") == 0){
+	$_SESSION["username"] = null;
+?>
 	<p> You have been logged out </p>
-	<?php 
-		}
-	    if (isset($_SESSION["username"]) && !is_null($_SESSION["username"]) || (isset($_POST["username"]) && isset($_POST["password"]) && strcmp($_POST["username"],"test") == 0 && strcmp($_POST["password"],"test") == 0)){
-	      $_SESSION["username"] = "test";
-	?>
+<?php 
+}
+if (isset($_SESSION["username"]) && !is_null($_SESSION["username"]) || (isset($_POST["username"]) && isset($_POST["password"]) && strcmp($_POST["username"],"test") == 0 && strcmp($_POST["password"],"test") == 0)){
+	$_SESSION["username"] = "test";
+?>
 		<p> Welcome to Politicks! You're resource for making change in the world! </p>
 		<form action="index.php" method="post">
 		<input type="hidden" name="logout" value="true" />
 
 		<input type="submit" value="Logout" data-theme="a"></input>
 	</form>	
-	<?php
-	    }else{         
-  	?>
+<?php
+}else{         
+?>
 	<form action="index.php" method="post">
 		<label for="basic">Username:</label>
 		<input type="text" name="username" id="basic" data-mini="true" />
@@ -28,12 +28,13 @@
 		<input type="password" name="password" id="basic" data-mini="true" />
 		<fieldset class="ui-grid-a">
 			<div class="ui-block-a"><button type="submit" data-theme="a">Login</button></div>
-			<div class="ui-block-b"><button type="submit" data-theme="b">Signup</button></div>
-	    </fieldset></button></div></button></div></fieldset>
+			<div class="ui-block-b"><a href='http://google.com'><button type="button" data-theme="b">Signup</button></a></div>
+		</fieldset></button></div></button></div></fieldset>
 	</form>	
-	<?php
-		}
-	?>
+	<p><a href='google.com'>Don't have an account? Sign up!</a>blahblah</p>
+<?php
+}
+?>
 </div><!-- /content -->
 
 <?php include_once("footer.php") ?>
