041abebd913a95d4355c9d228647dbe015b3bbab
cjanac <cjanac@gmail.com>
2012-11-15 01:14:32 -0800

    test

--- info.php
+++ info.php
@@ -3,14 +3,13 @@
 
 	<div data-role="content">	
 		<h1> About the App </h1>
-		<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio velit, non viverra dui. In hac habitasse platea dictumst. Nulla vitae eros leo. Vestibulum vitae lacus sapien, vitae consequat velit. Duis tristique pulvinar mauris, vitae feugiat elit viverra eu. Donec eget erat at sem luctus viverra eu et arcu. Curabitur dictum lorem nec eros mollis scelerisque. Pellentesque varius porttitor urna, sit amet auctor libero mollis non. </p>
+		<p> This app empowers groups political activists to pool their resources to influence issues they care about. It does so by allowing users to give money to an issue they care about. This money becomes a 'bounty' on that issue that can be claimed by a local politician who satisfactorily resolves this issue with a new public policy. The issue donation becomes powerful when an issue becomes viral within the site and many users donate to it. </p>
 
 		<h1> What do I Do? </h1>
-		<p>Duis sagittis iaculis condimentum. Maecenas viverra, mi pellentesque porttitor mollis, erat nibh scelerisque lorem, nec convallis nisi orci id dui. Duis in convallis lectus. Integer vel purus nisi. Nullam lobortis posuere lacus. Suspendisse potenti. Curabitur commodo nisl a nibh scelerisque commodo. Donec bibendum posuere volutpat. Suspendisse vel gravida elit. Phasellus et eros a mi fermentum pretium vel non sem. Etiam vehicula, dolor et auctor faucibus, purus libero convallis risus, eget condimentum sem eros sit amet orci.</p>
-		<a href="tutorial.php" data-role="button">View Tutorial</a>
-
-		<h1>FAQ</h1>
-		<p>Praesent id sapien sed dui rhoncus sollicitudin. Aenean at nulla sed tortor condimentum elementum in at odio. Maecenas rhoncus quam in dui aliquam porttitor. Ut mattis blandit interdum. Sed justo lectus, interdum vitae varius quis, sodales non neque. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent a tellus quis elit sodales pretium. Ut quis nisl neque, vel rutrum ipsum. Nam aliquet augue ante, eu lobortis libero. Maecenas placerat ante at lorem adipiscing et lobortis est consectetur.</p>
+		<p>As a user, search through the issues you care about. If you see one you like, donate to it! You won't need to give any money until the politician actually resolves the issue properly. 
+		  You can also create a new issue, which will require you to submit your credit card information and detailed info on the issue itself. You will be contacted if/when a local politician attempts to solve the issue with a policy.
+		  </p>
+		
 	</div><!-- /content -->
 
 <?php include_once("footer.php") ?>
