b72a83c3e6b8fb135ae47c48c197d8adbe351eba
Justin K. Chen <azn.justice@gmail.com>
2012-11-16 01:12:23 -0800

    added optimizely script at top

--- header.php
+++ header.php
@@ -12,6 +12,7 @@ require_once("config.php");
 	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
 	<script src="http://maps.google.com/maps/api/js?sensor=true" type="text/javascript"></script>
+	<script src="//cdn.optimizely.com/js/141417443.js"></script>
 </head> 
 <body> 
 
